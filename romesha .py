baa18779e7c00baf6123f46853a9637e55b10ecf
romeshatahir <romeshatahir332@gmail.com>
2025-04-14 18:12:51 +0500

    Add files via upload

---
+++ romesha .py
@@ -0,0 +1,66 @@
+# Q1: write program that calculates the sum,difference,product,and average of three numbers 
+a=10
+b=5
+c=3
+sum_result = a+b+c
+difference = a-b-c
+product = a*b*c
+average =sum_result /3
+
+print("sum",sum_result)
+print("difference",difference)
+print("product",product)
+print("average",average)
+
+
+print("OUTPUT")
+sum:18
+difference:2
+product:150
+average:6.0
+
+# Q2:write python code to calculate the area of the following shapes
+
+import math
+
+# Triangle
+base = 6
+height = 4
+area_triangle = 0.5 * base * height
+print("Area of Triangle:", area_triangle)
+
+# Square
+side = 5
+area_square = side * side
+print("Area of Square:", area_square)
+
+# Circle
+radius = 7
+area_circle = math.pi * radius * radius
+print("Area of Circle:", area_circle)
+
+# Rectangle
+length = 8
+width = 3
+area_rectangle = length * width
+print("Area of Rectangle:", area_rectangle)
+
+print ("output")
+print ("Area of Triangle: 12.0")  
+print ("Area of Square: 25")  
+print ("Area of Circle: 153.93804002589985")
+print ("Area of Rectangle: 24")
+
+
+# Q3:write a program to swap a values of two variables
+
+x=15
+y=30
+print("before swapping x =x,y,=y")
+x,y =y,x
+print("after swapping x = x,y =,y")
+
+
+
+
+
